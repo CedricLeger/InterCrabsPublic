59fdb7d0b0143cec18a827a7f74511b5f76e8862
SayoCrabs <47884342+SayoCrabs@users.noreply.github.com>
2020-09-04 03:11:09 +0200

    SBD login visiteur
    + angular material
    + html brouillon finis

--- InterCrabsFron/src/app/Login/login/login.component.ts
+++ InterCrabsFron/src/app/Login/login/login.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  pseudo : String;
+  mdp : String;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  inscription() 
+  {
+    //Quand le visiteur clique sur le boutton, on doit verifier que le mdp ne contient que des nombres
+    // et le pseudo pas de caratere speciaux
+    // il doit ensuite creer un nouvel utilisateur qui n'est pas activer
+    console.log("pseudo : " + this.pseudo + " mdp : " + this.mdp)
+  }
+
+  connexion()
+  {
+    //Quand l'utilisateur clique sur le bouton connexion, on doit regarder si il est bien en BDD et si il est bien activer 
+    console.log("pseudo : " + this.pseudo + " mdp : " + this.mdp)
+    
   }
 
 }
--- InterCrabsFron/src/app/app.module.ts
+++ InterCrabsFron/src/app/app.module.ts
@@ -4,6 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Login/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +24,18 @@ import { LoginComponent } from './Login/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
