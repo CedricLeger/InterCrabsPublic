ea5e3bc8156bd23aab9cee7e994e8f8a1bb7e716
SayoCrabs <47884342+SayoCrabs@users.noreply.github.com>
2020-09-04 14:06:52 +0200

    SBD start login service

--- InterCrabsFron/src/app/Login/login/login.component.ts
+++ InterCrabsFron/src/app/Login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import { LoginService } from './Service/login.service' ;
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  pseudo : String;
-  mdp : String;
+  pseudo = "";
+  mdp = "";
+  confirmMdp = "";
 
   constructor() { }
 
@@ -16,16 +18,30 @@ export class LoginComponent implements OnInit {
 
   inscription() 
   {
-    //Quand le visiteur clique sur le boutton, on doit verifier que le mdp ne contient que des nombres
+    // Quand le visiteur clique sur le boutton, on doit verifier que le mdp ne contient que des nombres
     // et le pseudo pas de caratere speciaux
-    // il doit ensuite creer un nouvel utilisateur qui n'est pas activer
+    if (this.pseudo.length == 0 || this.mdp.length == 0 || !this.confirmMdp.match(this.mdp)) {
+      //do nothing
+    }
+    else {
+    // il doit ensuite creer un nouvel utilisateur qui n'est pas activer SI l'utilisateur n'est pas deja inscrit en BDD 
     console.log("pseudo : " + this.pseudo + " mdp : " + this.mdp)
+    }
   }
 
   connexion()
   {
+    if (this.pseudo.length == 0 || this.mdp.length == 0) {
+      //do nothing
+    }
+    else {
     //Quand l'utilisateur clique sur le bouton connexion, on doit regarder si il est bien en BDD et si il est bien activer 
     console.log("pseudo : " + this.pseudo + " mdp : " + this.mdp)
+    }
+  }
+
+  validForm(pseudo : String, mdp : String)
+  {
     
   }
 
--- InterCrabsFron/src/app/Service/login.service.ts
+++ InterCrabsFron/src/app/Service/login.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = '';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })};
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(credentials): Observable<any> {
+    return this.http.post(AUTH_API + 'login', {
+      username: credentials.pseudo,
+      password: credentials.mdp
+    }, httpOptions);
+  }
+
+  register(user): Observable<any> {
+    return this.http.post(AUTH_API + 'login', {
+      username: user.pseudo,
+      password: user.mdp,
+    }, httpOptions);
+  }
 }
